a8cbe021ca23885c9c2112aebf87b56a909cbb77
Nathan Caza <mastercactapus@gmail.com>
2015-02-07 13:43:49 -0600

    add dhcpv6 file

---
+++ dhcpv6.go
@@ -0,0 +1,52 @@
+package dhcpv6
+
+import (
+	"bytes"
+	"encoding"
+	"encoding/binary"
+	"errors"
+	"io"
+	"net/http"
+)
+
+var ErrInvalidType = errors.New("Invalid type for message")
+var ErrInvalidIpv6Address = errors.New("Invalid IPv6 address")
+var ErrUnexpectedEOF = io.ErrUnexpectedEOF
+var ErrTooManyOptions = errors.New("No many options set to fit into buffer")
+var ErrDuidTooLong = errors.New("Duid exceeds maximum length of 128 octets")
+
+const (
+	//addresses
+	AddressAllDhcpServers               = "FF05::1:3"
+	AddressAllDhcpRelayAgentsAndServers = "FF02::1:2"
+
+	//Ports
+	PortClient = 546
+	PortServer = 547
+
+	//message types
+	_ = iota
+	TypeSolicit
+	TypeAdvertise
+	TypeRequest
+	TypeConfirm
+	TypeRenew
+	TypeRebind
+	TypeReply
+	TypeRelease
+	TypeDecline
+	TypeReconfigure
+	TypeInformationRequest
+	TypeRelayForward
+	TypeRelayReply
+
+	//Status Codes
+	Success = iota
+	UnspecFail
+	NoAddrsAvail
+	NoBinding
+	NotOnLink
+	UseMulticast
+
+	Infinity = 0xffffffff
+)
